314c41849b8778b6905c1d9f78db9c9ee31e2d65
solitoraynerussel <raynerussel.solito.eng@ust.edu.ph>
2019-03-26 18:27:12 +0800

    1st

---
+++ Problem1.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    ifstream inFile;
+    int priceOfBoxTicket, priceOfSidelineTicket, priceOfPremiumTicket,
+            priceOfGenAdmissionTicket;
+
+    int numOfBoxTicketSold,numOfSidelineTicketSold,numOfPremiumTicketSold,
+            numOfGenAdmissionTicketSold;
+
+    int amtOfBoxTicketSold, amtOfSidelineTicketSold, amtOfPremiumTicketSold,
+            amtOfGenAdmissionTicketSold;
+
+    long totalNumOfTicketsSold,totalSaleAmount;
+    cout << fixed << showpoint << setprecision(2);
+
+    inFile.open("TicketsSoldFile.txt");
+
+    inFile >> priceOfBoxTicket >> numOfBoxTicketSold
+           >> priceOfSidelineTicket >> numOfSidelineTicketSold
+           >> priceOfPremiumTicket >> numOfPremiumTicketSold
+           >> priceOfGenAdmissionTicket >> numOfGenAdmissionTicketSold;
+
+    amtOfBoxTicketSold = priceOfBoxTicket * numOfBoxTicketSold;
+    amtOfSidelineTicketSold = priceOfSidelineTicket * numOfSidelineTicketSold;
+    amtOfPremiumTicketSold = priceOfPremiumTicket * numOfPremiumTicketSold;
+    amtOfGenAdmissionTicketSold = priceOfGenAdmissionTicket * numOfGenAdmissionTicketSold;
+
+    totalNumOfTicketsSold = numOfBoxTicketSold + numOfSidelineTicketSold
+                            + numOfPremiumTicketSold + numOfGenAdmissionTicketSold;
+
+    totalSaleAmount = amtOfBoxTicketSold + amtOfSidelineTicketSold +
+                        amtOfPremiumTicketSold + amtOfGenAdmissionTicketSold;
+
+    cout << "Total number of Tickets sold is " << totalNumOfTicketsSold <<endl;
+    cout << "Total Sale Amount: " << totalSaleAmount << endl;
+
+    inFile.close();
+    return 0;
+}
+
